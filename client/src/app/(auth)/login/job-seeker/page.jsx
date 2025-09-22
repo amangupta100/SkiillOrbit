66108ea44b871c2568e859caf970073b5154498e
Aman Gupta <coderamang02@gmail.com>
2025-09-22 23:51:36 +0530

    change

--- client/src/app/(auth)/login/job-seeker/page.jsx
+++ client/src/app/(auth)/login/job-seeker/page.jsx
@@ -55,7 +55,7 @@ const page = () => {
           id: response.data.user.id,
           name: response.data.user.name,
         });
-        router.push("/userDashboard");
+        router.replace("/userDashboard");
       } else {
         toast.error(message);
       }
