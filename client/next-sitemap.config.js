f2e822e409303b969577e90c250e472e3603786b
Aman Gupta <coderamang02@gmail.com>
2025-11-07 19:11:18 +0530

    edited sitemap.config.js

--- client/next-sitemap.config.js
+++ client/next-sitemap.config.js
@@ -1,5 +1,8 @@
-module.exports = {
-  siteUrl: "https://skillsorbit.in",
-  generateRobotsTxt: true,
-  outDir: "public",
+/** @type {import('next-sitemap').IConfig} */
+const config = {
+  siteUrl: "https://skillsorbit.in", // Your actual domain
+  generateRobotsTxt: true, // Optional: Also generates robots.txt
+  sitemapSize: 5000, // Split large sitemaps if needed
 };
+
+export default config;
